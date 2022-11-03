580abc6e56ebeb089844c936ed85aa8a1fb0c62d
Psycomentis06 <alibenamor30@gmail.com>
2022-11-03 22:54:21 +0100

    Getting started

---
+++ src/main/java/com/github/psycomentis06/isatexbackendservice/IsatexBackendServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.psycomentis06.isatexbackendservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsatexBackendServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsatexBackendServiceApplication.class, args);
+    }
+
+}
