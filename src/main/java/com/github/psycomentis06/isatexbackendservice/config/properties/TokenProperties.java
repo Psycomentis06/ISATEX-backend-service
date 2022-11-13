3add35e8b75f270434a5cccda6fd8cedf60cb5aa
Psycomentis06 <alibenamor30@gmail.com>
2022-11-13 22:55:32 +0100

    Basic

---
+++ src/main/java/com/github/psycomentis06/isatexbackendservice/config/properties/TokenProperties.java
@@ -0,0 +1,9 @@
+package com.github.psycomentis06.isatexbackendservice.config.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties()
+public class TokenProperties {
+}
