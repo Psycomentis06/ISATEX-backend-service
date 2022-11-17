5e653c234bb7ee0c8ff33317060fbde09049a4d6
Psycomentis06 <alibenamor30@gmail.com>
2022-11-18 00:29:03 +0100

    InputStream Wrapper

---
+++ src/main/java/com/github/psycomentis06/isatexbackendservice/filter/CachedBodyHttpServletRequestFilter.java
@@ -0,0 +1,25 @@
+package com.github.psycomentis06.isatexbackendservice.filter;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@Component
+@WebFilter(filterName = "CachedBodyHttpServletRequestFilter", urlPatterns = "/*")
+public class CachedBodyHttpServletRequestFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        System.out.println("Cache filter");
+        RequestWrapper requestWrapper = new RequestWrapper(request);
+        filterChain.doFilter(requestWrapper, response);
+    }
+}
---
+++ src/main/java/com/github/psycomentis06/isatexbackendservice/filter/RequestWrapper.java
@@ -0,0 +1,22 @@
+package com.github.psycomentis06.isatexbackendservice.filter;
+
+import org.springframework.util.StreamUtils;
+
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import java.io.IOException;
+
+public class RequestWrapper extends HttpServletRequestWrapper {
+    private byte[] body;
+
+    public RequestWrapper(HttpServletRequest request) throws IOException {
+        super(request);
+        this.body = StreamUtils.copyToByteArray(request.getInputStream());
+    }
+
+    @Override
+    public ServletInputStream getInputStream() throws IOException {
+        return new ServletInputStreamWrapper(body);
+    }
+}
