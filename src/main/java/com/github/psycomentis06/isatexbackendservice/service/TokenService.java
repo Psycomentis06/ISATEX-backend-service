64d2c3a8f456aa1764fef88f50fbb40191a7616e
Psycomentis06 <alibenamor30@gmail.com>
2022-11-20 18:09:50 +0100

    Access token

---
+++ src/main/java/com/github/psycomentis06/isatexbackendservice/service/TokenService.java
@@ -0,0 +1,37 @@
+package com.github.psycomentis06.isatexbackendservice.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Service;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Service
+public class TokenService {
+
+    @Value("${jwt.public.key}")
+    RSAPublicKey publicKey;
+
+    @Value("${jwt.private.key}")
+    RSAPrivateKey privateKey;
+
+   public String generateAccessToken(Authentication authentication, String issuer) {
+       Algorithm algorithm = Algorithm.RSA256(publicKey, privateKey);
+       User user = (User) authentication.getPrincipal();
+       return JWT.create()
+               .withSubject(user.getUsername())
+               .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
+               .withIssuer(issuer)
+               .withClaim("roles",
+                      user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList())
+                       )
+               .sign(algorithm);
+   }
+}
