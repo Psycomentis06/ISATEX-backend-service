de68a7eeb45303c5931a2f3e9e63723cbc1c0bbc
Psycomentis06 <alibenamor30@gmail.com>
2022-11-20 18:09:21 +0100

    Base data

--- src/main/java/com/github/psycomentis06/isatexbackendservice/IsatexBackendServiceApplication.java
+++ src/main/java/com/github/psycomentis06/isatexbackendservice/IsatexBackendServiceApplication.java
@@ -1,13 +1,29 @@
 package com.github.psycomentis06.isatexbackendservice;
 
+import com.github.psycomentis06.isatexbackendservice.service.BaseDBData;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class IsatexBackendServiceApplication {
+
+    private final BaseDBData baseDBData;
+
+    public IsatexBackendServiceApplication(BaseDBData baseDBData) {
+        this.baseDBData = baseDBData;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(IsatexBackendServiceApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner defaultData() {
+        return args -> {
+            baseDBData.saveRequiredRoles();
+            baseDBData.saveRequiredUser();
+        };
+    }
 }
---
+++ src/main/java/com/github/psycomentis06/isatexbackendservice/service/BaseDBData.java
@@ -0,0 +1,60 @@
+package com.github.psycomentis06.isatexbackendservice.service;
+
+import com.github.psycomentis06.isatexbackendservice.entity.Role;
+import com.github.psycomentis06.isatexbackendservice.entity.User;
+import com.github.psycomentis06.isatexbackendservice.repository.RoleRepository;
+import com.github.psycomentis06.isatexbackendservice.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Service
+public class BaseDBData {
+    private final RoleRepository roleRepository;
+    private final UserService userService;
+
+    public BaseDBData(RoleRepository roleRepository, UserService userService) {
+        this.roleRepository = roleRepository;
+        this.userService = userService;
+    }
+
+    @Transactional
+    public void saveRequiredRoles() {
+        ArrayList<Role> roles = new ArrayList<>();
+        roles.add(new Role(Role.ROLE_FORWARDING_AGENT));
+        roles.add(new Role(Role.ROLE_ADMIN));
+        roles.add(new Role(Role.ROLE_CUSTOMER));
+        roles.add(new Role(Role.ROLE_SECRETARY));
+        roles.add(new Role(Role.ROLE_STOREKEEPER));
+        roles.add(new Role(Role.ROLE_PRIVILEGED_CUSTOMER));
+        roles.add(new Role(Role.ROLE_CUSTOMS_OFFICER));
+
+       roleRepository.saveAll(roles);
+    }
+
+    @Transactional
+    public void saveRequiredUser() {
+        User user = new User();
+        user
+                .setEmail("admin@isatex.tn")
+                .setPassword("123456789")
+                .setUsername("admin")
+                .setFirstName("admin")
+                .setLastName("admin");
+
+        Optional<Role> adminRole = roleRepository.findById(Role.ROLE_ADMIN);
+        Role role;
+        if (adminRole.isEmpty()) {
+            role = roleRepository.save(new Role(Role.ROLE_ADMIN));
+        } else {
+            role = adminRole.get();
+        }
+        ArrayList<Role> roles = new ArrayList<>();
+        roles.add(role);
+        user.setRoles(roles);
+
+        userService.createUser(user);
+    }
+}
