3bad6126ff14d9fe1e0c377eccdeb8ca712baeba
Psycomentis06 <alibenamor30@gmail.com>
2022-11-26 17:10:40 +0100

    Constraints

--- src/main/java/com/github/psycomentis06/isatexbackendservice/entity/User.java
+++ src/main/java/com/github/psycomentis06/isatexbackendservice/entity/User.java
@@ -4,10 +4,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.Accessors;
+import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.UniqueElements;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -27,11 +29,15 @@ public class User {
 
     private String lastName;
 
+    @NotBlank(message = "Username is required")
     @Column(unique = true, nullable = false)
     private String username;
 
+    @NotBlank(message = "Password is required")
+    @Length(min = 6, max = 16, message = "Password length should be greater than 6 and less than 16")
     private String password;
 
+    @NotBlank(message = "Email is required")
     @Email(message = "Invalid email format")
     @Column(unique = true, nullable = false)
     private String email;
