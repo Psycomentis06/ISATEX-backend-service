5c107104a8b5a2442a8d4607b9da17ade78b30f2
Psycomentis06 <alibenamor30@gmail.com>
2022-11-26 17:11:33 +0100

    Reset password

--- src/main/java/com/github/psycomentis06/isatexbackendservice/controller/api/AuthController.java
+++ src/main/java/com/github/psycomentis06/isatexbackendservice/controller/api/AuthController.java
@@ -1,20 +1,25 @@
 package com.github.psycomentis06.isatexbackendservice.controller.api;
 
+import com.github.psycomentis06.isatexbackendservice.entity.User;
+import com.github.psycomentis06.isatexbackendservice.form.EmailForm;
 import com.github.psycomentis06.isatexbackendservice.form.LoginForm;
+import com.github.psycomentis06.isatexbackendservice.service.EmailService;
 import com.github.psycomentis06.isatexbackendservice.service.RedisService;
 import com.github.psycomentis06.isatexbackendservice.service.TokenService;
+import com.github.psycomentis06.isatexbackendservice.service.UserService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -24,19 +29,28 @@ public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final TokenService tokenService;
 
-    private RedisService redisService;
+    private final RedisService redisService;
 
-    public AuthController(AuthenticationManager authenticationManager, TokenService tokenService, RedisService redisService) {
+    private final UserService userService;
+
+    private final EmailService emailService;
+
+    @Value("${app.auth.reset-password-url}")
+    private String resetPasswordUrl;
+
+    public AuthController(AuthenticationManager authenticationManager, TokenService tokenService, RedisService redisService, UserService userService, EmailService emailService) {
         this.authenticationManager = authenticationManager;
         this.tokenService = tokenService;
         this.redisService = redisService;
+        this.userService = userService;
+        this.emailService = emailService;
     }
 
     @PostMapping("/login")
     public ResponseEntity<Object> authenticate(@RequestBody LoginForm loginForm, HttpServletRequest request) {
         UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(loginForm.getUsername(), loginForm.getPassword());
-         Authentication authentication = authenticationManager.authenticate(token);
-        Map<String , String> resp = new HashMap<>();
+        Authentication authentication = authenticationManager.authenticate(token);
+        Map<String, String> resp = new HashMap<>();
         resp.put("accessToken", tokenService.generateAccessToken(authentication, request.getRequestURL().toString()));
         resp.put("refreshToken", tokenService.generateRefreshToken(authentication, request.getRequestURL().toString()));
         return new ResponseEntity<>(resp, null, HttpStatus.OK);
@@ -44,14 +58,36 @@ public class AuthController {
 
     @PostMapping("/password/reset/request")
     public ResponseEntity<Object> sendResetRequest(
-        @RequestBody String email
-    )  {
-        byte[] array = new byte[7]; // length is bounded by 7
-        new Random().nextBytes(array);
-        String generatedString = new String(array, StandardCharsets.UTF_8);
-        String token = UUID.randomUUID().toString();
-       redisService.setUserResetPasswordToken("1", generatedString);
-        return new ResponseEntity<>(token, null, HttpStatus.OK);
+            @RequestBody String email
+    ) {
+        Optional<User> user = userService.getUser(User.class, "", email);
+        user.ifPresentOrElse(u -> {
+            String token = UUID.randomUUID().toString();
+            EmailForm emailForm = new EmailForm();
+            String resetUrl = resetPasswordUrl + "?id=" + u.getId() + "&token=" + token;
+            emailForm
+                    .setSubject("Reset Password")
+                    .setRecipient(email)
+                    .setMessageBody(
+                            "Reset password link:" +
+                                    resetUrl
+                    );
+            emailService.sendEmail(emailForm);
+            redisService.setUserResetPasswordToken(Integer.toString(u.getId()), token);
+        }, () -> {
+            throw new UsernameNotFoundException("User not found");
+        });
+        Map<String, String> resp = new HashMap<>();
+        resp.put("message", "Reset password request sent to given Email");
+        return new ResponseEntity<>(resp, null, HttpStatus.OK);
+    }
+
+    @PostMapping("/password/reset")
+    public ResponseEntity<Object> resetPassword(
+            @RequestParam(name = "id", defaultValue = "", required = true) String email,
+            @RequestParam(name = "token", defaultValue = "", required = true) String token
+    ) {
+        return new ResponseEntity<>("res", null, HttpStatus.OK);
     }
 
 }
