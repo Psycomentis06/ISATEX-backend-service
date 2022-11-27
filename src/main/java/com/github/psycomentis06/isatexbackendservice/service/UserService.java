b73cb139a91457e5e7d0b042927cbc8891c2fa37
Psycomentis06 <alibenamor30@gmail.com>
2022-11-27 18:05:46 +0100

    Check pass

--- src/main/java/com/github/psycomentis06/isatexbackendservice/service/UserService.java
+++ src/main/java/com/github/psycomentis06/isatexbackendservice/service/UserService.java
@@ -3,6 +3,7 @@ package com.github.psycomentis06.isatexbackendservice.service;
 import com.github.psycomentis06.isatexbackendservice.entity.Customer;
 import com.github.psycomentis06.isatexbackendservice.entity.Role;
 import com.github.psycomentis06.isatexbackendservice.entity.User;
+import com.github.psycomentis06.isatexbackendservice.exception.InvalidPasswordException;
 import com.github.psycomentis06.isatexbackendservice.repository.CustomerRepository;
 import com.github.psycomentis06.isatexbackendservice.repository.RoleRepository;
 import com.github.psycomentis06.isatexbackendservice.repository.UserRepository;
@@ -10,9 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 @Transactional
@@ -33,16 +32,41 @@ public class UserService {
     }
 
     public void createUser(User user) {
+        checkPassword(user.getPassword());
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
+    }
+
+
+    public void checkPassword(String password) {
+        List<String> constraints = new ArrayList<>();
+        if (password == null) {
+            constraints.add("Password is missing");
+        } else {
+            if (password.length() > 16) {
+                constraints.add("Password length bigger than 16");
+            }
+
+            if (password.length() < 6) {
+                constraints.add("Password length shorter than 6");
+            }
+        }
+
+        if (constraints.size() > 0) {
+            throw new InvalidPasswordException("Malformed password", 400, constraints);
+        }
     }
 
     public void createCustomer(Customer customer) {
         createCustomer(customer, false);
     }
+
     public void createCustomer(Customer customer, boolean verified) {
+        checkPassword(customer.getPassword());
         customer.setVerified(verified);
         customer.setPassword(passwordEncoder.encode(customer.getPassword()));
+        System.out.println("PLength=" + customer.getPassword().length());
+        System.out.println(customer.getPassword());
         ArrayList<Role> roles = new ArrayList<>();
         Optional<Role> role = roleRepository.findById(Role.ROLE_CUSTOMER);
         role.ifPresent(r -> {
@@ -72,5 +96,11 @@ public class UserService {
 
     public <T> Optional<T> getUser(Class<T> classType, String username, String email) {
         return userRepository.findUserByUsernameOrEmail(classType, username, email);
+    }
+
+    public <T> Optional<T> getUserByUsernameOrPassword(Class<T> classType, String value) {
+        Optional<T> userOptional = userRepository.findByUsername(classType, value);
+        if (userOptional.isPresent()) return userOptional;
+        return userRepository.findByEmail(classType, value);
     }
 }
