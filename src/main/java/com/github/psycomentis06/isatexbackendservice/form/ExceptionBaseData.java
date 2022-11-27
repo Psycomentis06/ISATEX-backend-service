1e166124c82e85f35e86bc811576bf2c754428bf
Psycomentis06 <alibenamor30@gmail.com>
2022-11-27 18:05:26 +0100

    Lombok chaining

--- src/main/java/com/github/psycomentis06/isatexbackendservice/form/ExceptionBaseData.java
+++ src/main/java/com/github/psycomentis06/isatexbackendservice/form/ExceptionBaseData.java
@@ -3,8 +3,10 @@ package com.github.psycomentis06.isatexbackendservice.form;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors(chain = true)
 @NoArgsConstructor
 public class ExceptionBaseData {
     private String message;
