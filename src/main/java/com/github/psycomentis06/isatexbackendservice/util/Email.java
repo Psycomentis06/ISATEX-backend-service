6145017659eb5a12622a8fd1ad688256c2c909dd
Psycomentis06 <alibenamor30@gmail.com>
2022-11-27 18:09:20 +0100

    Not Implemented for now

---
+++ src/main/java/com/github/psycomentis06/isatexbackendservice/util/Email.java
@@ -0,0 +1,12 @@
+package com.github.psycomentis06.isatexbackendservice.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Email {
+    public static boolean isEmail(String value) {
+        //Pattern pattern = Pattern.compile("/[^\S]*@[a-z0-9.-]*/i");
+        //Matcher matcher = pattern.matcher(value);
+return true;
+    }
+}
