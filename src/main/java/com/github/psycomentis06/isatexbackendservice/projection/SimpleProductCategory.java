b7d92bad6842bfec0a6e9f90fb1354b0e5b846a7
Psycomentis06 <alibenamor30@gmail.com>
2022-11-28 23:01:35 +0100

    getId

--- src/main/java/com/github/psycomentis06/isatexbackendservice/projection/SimpleProductCategory.java
+++ src/main/java/com/github/psycomentis06/isatexbackendservice/projection/SimpleProductCategory.java
@@ -1,11 +1,13 @@
 package com.github.psycomentis06.isatexbackendservice.projection;
 
-import com.github.psycomentis06.isatexbackendservice.entity.ProductCategory;
-
 import java.util.List;
 
 public interface SimpleProductCategory {
+    int getId();
+
     String getName();
+
     SimpleProductCategory getParent();
+
     List<SimpleProductCategoryName> getCategories();
 }
--- src/main/java/com/github/psycomentis06/isatexbackendservice/projection/SimpleProductCategoryName.java
+++ src/main/java/com/github/psycomentis06/isatexbackendservice/projection/SimpleProductCategoryName.java
@@ -1,5 +1,6 @@
 package com.github.psycomentis06.isatexbackendservice.projection;
 
 public interface SimpleProductCategoryName {
+    int getId();
     String getName();
 }
