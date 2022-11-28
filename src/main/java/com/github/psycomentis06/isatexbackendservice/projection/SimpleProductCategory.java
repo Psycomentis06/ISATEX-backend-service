44039445d2fe6e24279f69562d901bc8cccb15ed
Psycomentis06 <alibenamor30@gmail.com>
2022-11-28 23:15:40 +0100

    Simplified

--- src/main/java/com/github/psycomentis06/isatexbackendservice/projection/SimpleProductCategory.java
+++ src/main/java/com/github/psycomentis06/isatexbackendservice/projection/SimpleProductCategory.java
@@ -1,13 +1,9 @@
 package com.github.psycomentis06.isatexbackendservice.projection;
 
-import java.util.List;
-
 public interface SimpleProductCategory {
     int getId();
 
     String getName();
 
     SimpleProductCategory getParent();
-
-    List<SimpleProductCategoryName> getCategories();
 }
