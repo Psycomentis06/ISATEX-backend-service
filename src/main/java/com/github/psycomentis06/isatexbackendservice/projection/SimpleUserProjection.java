fef719716273eeb4fe088db3803c0de350f4a884
Psycomentis06 <alibenamor30@gmail.com>
2022-11-30 21:22:45 +0100

    Correct getFirstName

--- src/main/java/com/github/psycomentis06/isatexbackendservice/projection/SimpleUserProjection.java
+++ src/main/java/com/github/psycomentis06/isatexbackendservice/projection/SimpleUserProjection.java
@@ -1,12 +1,16 @@
 package com.github.psycomentis06.isatexbackendservice.projection;
 
+import com.github.psycomentis06.isatexbackendservice.entity.Role;
+
+import java.util.List;
+
 public interface SimpleUserProjection {
-    String getId();
-    String getFistName();
+    int getId();
+    String getFirstName();
     String getLastName();
     String getUsername();
     String getEmail();
-    String getVerified();
-    String getRoles();
+    boolean isVerified();
     String getImage();
+    List<Role> getRoles();
 }
