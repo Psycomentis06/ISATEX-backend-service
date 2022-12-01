fd370f628bd58989634693918fdddbe6de36dc4b
Psycomentis06 <alibenamor30@gmail.com>
2022-12-01 23:06:40 +0100

    Purchase Date

--- src/main/java/com/github/psycomentis06/isatexbackendservice/projection/SimpleMachineProjection.java
+++ src/main/java/com/github/psycomentis06/isatexbackendservice/projection/SimpleMachineProjection.java
@@ -1,6 +1,10 @@
 package com.github.psycomentis06.isatexbackendservice.projection;
 
+import java.util.Date;
+
 public interface SimpleMachineProjection {
     int getId();
     String getName();
+
+    Date getPurchaseDate();
 }
